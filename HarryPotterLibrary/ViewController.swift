e8e043036daf004d3f15df3cca835dd2beafaf44
Gwangyong <key4168@icloud.com>
2025-06-13 20:52:17 +0900

    chore: 코드베이스 UI 구조 및 SnapKit 초기 설정

---
+++ HarryPotterLibrary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HarryPotterLibrary
+//
+//  Created by 서광용 on 6/13/25.
+//
+import SnapKit
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+}
+
